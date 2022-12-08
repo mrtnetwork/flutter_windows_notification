d11888eca28586d8f9ad40d296c1f92fc6e9d95e
Mohsen <56779182+MohsenHaydari@users.noreply.github.com>
2022-12-08 12:11:51 +0330

    Update generated_plugin_registrant.cc

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,18 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <screen_retriever/screen_retriever_plugin.h>
-#include <system_tray/system_tray_plugin.h>
-#include <window_manager/window_manager_plugin.h>
 #include <windows_notification/windows_notification_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ScreenRetrieverPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
-  SystemTrayPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SystemTrayPlugin"));
-  WindowManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
   WindowsNotificationPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowsNotificationPluginCApi"));
 }
